0ff282f479f14637d4216cb1e34e83ec9ecb794a
Anish <anish.shrestha@smarten.com.np>
2022-02-21 17:00:24 +0545

    add customer module

---
+++ customer/__manifest__.py
@@ -0,0 +1,21 @@
+from email.mime import application
+from msilib import sequence
+from unicodedata import category
+
+
+{
+    'name': "Customers",
+    'version':"15.0.0.1",
+    'category':"Extra Tools",
+    'summary': "Customer Differentiation modules",
+    'sequence': 1,
+    'author': "Anish Shrestha",
+    'maintainer': "Anish Shrestha",
+    'website': "anishshrestha.dev",
+    'depends': [],
+    'demo': [],
+    'data':[],
+    'installabe':True,
+    'application':True,
+    'auto_install':False
+}
